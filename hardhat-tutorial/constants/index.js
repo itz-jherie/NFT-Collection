0a3a11f82a6300d88164253089cf32a581665368
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2022-07-02 03:35:16 +0100

    initial comit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xAA5bCF842CD82f1349E2eA43e2Dd16bf8b223091";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
